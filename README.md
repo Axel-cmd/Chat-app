f37ab2e1828355714290e2281c22a5b3efa6bbfd
AXELMINART <77530883+Axel-cmd@users.noreply.github.com>
2022-10-18 13:21:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chat App
+ Projet application de chat 
---
+++ backend/config/config.ts
@@ -0,0 +1,5 @@
+export default {
+    port: 8080,
+    host: "localhost",
+    dbUri: "mongodb+srv://axel:haFhbca91@cluster0.rsqvv.mongodb.net/fullstackbackend?retryWrites=true&w=majority"
+}
